0e678989bc48d1798243727466108914b8303048
Raj Gupta <116415795+Rajg2901@users.noreply.github.com>
2023-04-26 23:14:02 +0530

    Add files via upload
    Program to find index of a given number by user from an unsorted array.

---
+++ Find index from array.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+//code to find the index of in a given number from a array
+
+int binary(int arr[], int size, int num)
+{
+
+    int start = 0;
+    int end = size - 1;
+int mid;
+    mid = (start + end) / 2;
+
+    while (start <= end)
+    {
+
+        if (arr[mid] == num)
+        {
+
+            return mid;
+        }
+
+        if (arr[mid] < num)
+        {
+
+            // go right
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+         mid = (start + end) / 2;
+    }
+    return -1;
+}
+
+int main()
+{
+
+    int arr1[5] = {2,5,7,8,25};
+    int arr2[4] = {33, 6, 7, 13};
+
+    
+int index = binary(arr1,5,8);
+
+   cout<< "index is"<< index;
+
+    return 0;
+}
